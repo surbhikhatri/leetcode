4e4336ff7263fefcc637e56a79553c3b364aa12f
surbhikhatri <47811836+surbhikhatri@users.noreply.github.com>
2024-06-18 16:08:40 -0700

    Time: 43 ms (22.08%) | Memory: 16.5 MB (96.40%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,19 +1,12 @@
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        minlen = min([len(s) for s in strs])
-
         res = ""
-        for i in range(minlen):
-            fnd = True
-            for j in range(1, len(strs)):
-                if strs[j][i] != strs[j - 1][i]:
-                    fnd = False
-            
-            if fnd == True:
-                res += strs[0][i]
-            else:
-                break
+
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
         
         return res
-
         
