91dbbeb08f593332e3d8f67a93bcce3161a0335d
surbhikhatri <47811836+surbhikhatri@users.noreply.github.com>
2024-06-18 17:49:28 -0700

    Time: 50 ms (69.80%) | Memory: 16.5 MB (92.20%) - LeetSync

--- 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
+++ 2724-convert-an-array-into-a-2d-array-with-conditions/convert-an-array-into-a-2d-array-with-conditions.py
@@ -1,13 +1,13 @@
 class Solution:
     def findMatrix(self, nums: List[int]) -> List[List[int]]:
-        # 1D to 2D array, distinct in each row, #rows minimum
+        # 1D to 2D array, distinct in each row, # rows minimum
+        # find the maximum number of time an element is repeated, it will decide the # of rows
 
-        nums.sort()
         fmap = defaultdict(int)
         for n in nums:
             fmap[n] += 1
         r = max(fmap.values())
-        res = [[] for _ in range(r)] # max duplicates, # of rows
+        res = [[] for _ in range(r)] 
 
         j = 0
         for k, v in fmap.items():
