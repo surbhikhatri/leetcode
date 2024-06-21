d6856ac2f2c21dcacdd848638b0ef9f7f16721a7
surbhikhatri <47811836+surbhikhatri@users.noreply.github.com>
2024-06-20 23:43:25 -0700

    Time: 303 ms (55.82%) | Memory: 16.6 MB (98.04%) - LeetSync

--- 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
+++ 1263-number-of-dice-rolls-with-target-sum/number-of-dice-rolls-with-target-sum.py
@@ -2,7 +2,26 @@ class Solution:
     def numRollsToTarget(self, n: int, k: int, target: int) -> int:
         # dp to store state: (dice remaining, cur sum)
         # n dice -> k faces from 1 to k, target, return # of possible ways out of k**n 
-        # % 10 ** 9 + 7
+        dp = [0] * (target + 1) # number of ways to roll i
+        dp[0] = 1
+        mod = 10 ** 9 + 7
+        for dice in range(n): # num of dice
+            next_dp = [0] * (target + 1)
+
+            for val in range(1, k + 1): # k faces
+                for total in range(val, target + 1): # look at all the values in dp array
+                    # total = 8 ==  ways to roll 3 + ways to roll 5
+                    next_dp[total] = (next_dp[total] + dp[total - val]) % mod
+            
+            dp = next_dp
+        
+        return dp[target]
+
+
+
+
+
+
         mod = 10 ** 9 + 7
         cache = {}
         def count(n, target):
@@ -19,7 +38,7 @@ class Solution:
             cache[(n, target)] = res
             return res
         
-        return count(n, target)     
+        return count(n, target)  # top down    
             
 
 
